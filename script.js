d817144257ef49bbb97455c004d7bd48038478fa
Olimjon <hamdamovolimjon1997@gmail.com>
2023-09-30 15:17:35 +0500

    first

---
+++ script.js
@@ -0,0 +1,12 @@
+let navBtn = document.querySelector('.burger')
+let navList = document.querySelector('.list')
+
+navBtn.addEventListener('click', btn)
+function btn() {
+    this.classList.toggle('burger__active')
+    if (navBtn.classList.contains('burger__active')) {
+        navList.classList.add('active')
+    } else if (!navBtn.classList.contains('burger__active')) {
+        navList.classList.remove('active')
+    }
+}
